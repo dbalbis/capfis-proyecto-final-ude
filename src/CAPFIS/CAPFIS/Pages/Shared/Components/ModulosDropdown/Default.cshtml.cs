b1b7a542f71381fcea926651ef99470f5cf608af
Diego Balbis <dibal@frontmatec.com>
2025-09-09 11:25:29 -0300

    Terminado el renderizado dinamico de modulos en menu

---
+++ src/CAPFIS/CAPFIS/Pages/Shared/Components/ModulosDropdown/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CAPFIS.Pages.Shared.Components.ModulosDropdown
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/CAPFIS/CAPFIS/ViewComponents/ModulosDropdownViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using CAPFIS.Data;
+using CAPFIS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CAPFIS.ViewComponents
+{
+    public class ModulosDropdownViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ModulosDropdownViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var modulos = await _context.Modulos
+                .Where(m => m.EstaPublicado)
+                .OrderBy(m => m.Orden)
+                .ToListAsync();
+
+            return View(modulos); // esto enviará la lista al Default.cshtml
+        }
+    }
+}
